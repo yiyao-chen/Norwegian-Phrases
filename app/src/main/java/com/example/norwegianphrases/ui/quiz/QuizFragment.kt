d44c52a58ddf7a9449116a89f4972b209a5dff4d
yiyaochen <yiyaoc@uio.no>
2021-07-15 20:30:02 +0200

    options clickable and in random order

--- app/src/main/java/com/example/norwegianphrases/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/example/norwegianphrases/ui/quiz/QuizFragment.kt
@@ -1,5 +1,6 @@
 package com.example.norwegianphrases.ui.quiz
 
+import android.graphics.Color
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,15 +8,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import com.example.norwegianphrases.ActivityViewModel
+import com.example.norwegianphrases.R
 import com.example.norwegianphrases.database.Phrase
 import com.example.norwegianphrases.databinding.QuizFragmentBinding
+import kotlinx.android.synthetic.main.quiz_fragment.*
 
-class QuizFragment : Fragment() {
+class QuizFragment : Fragment(), View.OnClickListener {
 
     private val viewModel: ActivityViewModel by activityViewModels()
     private var binding: QuizFragmentBinding? = null
+
+
 
     var phrases = listOf<Phrase>() // all phrases
 
@@ -23,6 +29,8 @@ class QuizFragment : Fragment() {
    // var noPhrases = listOf<Phrase>() // quiz alternatives / norwegian phrases
     var chTrans = listOf<Phrase>() // answer alternatives/ only chinese translations
 
+    var mCurrentQuizPos: Int = 0
+    var mSelectedOptionPos : Int = 0
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -35,12 +43,14 @@ class QuizFragment : Fragment() {
 
         selectQuizes() // randomly select phrases from collection
 
-        for(p in quizList) {
-            println("::: "+p.phrase)
-        }
+        val firstQuiz = displayFirstQuiz()
+        displayOptions(firstQuiz) // for first quiz
 
-        displayFirstQuiz()
-        displayOptions() // for first quiz
+        binding!!.optionOne.setOnClickListener(this)
+        binding!!.optionTwo.setOnClickListener(this)
+        binding!!.optionThree.setOnClickListener(this)
+
+
         return root
     }
 
@@ -48,7 +58,7 @@ class QuizFragment : Fragment() {
         val phraseList : ArrayList<Phrase> = arrayListOf() // collection of options
         phraseList.addAll(phrases)
 
-        for(i in 1..2) {
+        for(i in 1..3) {
             val randomIndex = (phraseList.indices).random()
             // add randomly selected quiz to quizList, and remove from collection to avoid duplicates
             val p = phraseList[randomIndex]
@@ -59,45 +69,78 @@ class QuizFragment : Fragment() {
 
     }
     //display random phrase/quiz
-    fun displayFirstQuiz() {
+    fun displayFirstQuiz() : Phrase {
         val quizPhrase: TextView = binding!!.quizPhrase
         quizPhrase.text = quizList[0].phrase
-
+        return quizList[0]
     }
+
     // display answer options
-    fun displayOptions() {
+    fun displayOptions(currentQuiz : Phrase) {
         val phraseList : ArrayList<Phrase> = arrayListOf() // collection of options
         phraseList.addAll(phrases)
+
+        val optionsList : ArrayList<Phrase> = arrayListOf()
+        optionsList.add(currentQuiz) // add currect option to list
+
+        phraseList.remove(currentQuiz) // remove from options to avoid duplicates
+
+        // add two randomly selected options to list
+        for(i in 1..2) {
+            val randomIndex = (phraseList.indices).random()
+            val p = phraseList[randomIndex]
+            optionsList.add(p)
+            phraseList.remove(p)
+        }
+
+        val shuffledList = optionsList.shuffled()
+        println("optionlist: ")
+        for(p in optionsList) {
+            println("::: "+p.chTrans)
+        }
+        println("shuffled optionlist: ")
+        for(p in shuffledList) {
+            println(":---:: "+p.chTrans)
+        }
 
         val option1 = binding!!.optionOne
         val option2 = binding!!.optionTwo
         val option3 = binding!!.optionThree
 
-        // assign correct answer to option 1
-        option1.text = quizList[0].chTrans
-
-        // assign random values to option 2
-        phraseList.remove(quizList[0]) // remove the correct option from collection to avoid duplicates
-            var randomIndex = (phraseList.indices).random()
-            // add randomly selected quiz as option, and remove from collection to avoid duplicates
-            var p = phraseList[randomIndex]
-            option2.text = p.chTrans
-            phraseList.remove(p)
-
-        // assign random values to option 3
-        randomIndex = (phraseList.indices).random()
-        // add randomly selected quiz as option, and remove from collection to avoid duplicates
-        p = phraseList[randomIndex]
-        option3.text = p.chTrans
-        phraseList.remove(p)
-
-
-
+        // set text
+        option1.text = shuffledList[0].chTrans
+        option2.text = shuffledList[1].chTrans
+        option3.text = shuffledList[2].chTrans
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         binding = null
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.option_one -> {
+                selectedOptionView(option_one, 1)
+            }
+            R.id.option_two -> {
+                selectedOptionView(option_two, 2)
+            }
+            R.id.option_three -> {
+                selectedOptionView(option_three, 3)
+            }
+        }
+    }
+
+    fun selectedOptionView(tv: TextView, selectedOptionNum: Int) {
+        mSelectedOptionPos = selectedOptionNum
+        tv.setTextColor(Color.parseColor("#FFBB86FC"))
+        if(tv.text == quizList[mCurrentQuizPos].chTrans) {
+            Toast.makeText(activity,"RRR",Toast.LENGTH_SHORT).show()
+        } else if(tv.text != quizList[mCurrentQuizPos].chTrans) {
+            Toast.makeText(activity,"false",Toast.LENGTH_SHORT).show()
+
+        }
     }
 
 }
